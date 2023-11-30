535322af6826637a18aec2ba3437764d705b90fa
loak20 <aryankadam2055@gmail.com>
2023-11-30 16:48:38 +0530

    update

--- q7.cpp
+++ q7.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std ;
 int main(){
-    int n ;
+    long long int n ;
     cin>>n ; 
-    for(int k = 1 ;k<=n ;k++){
+    for(long long int k = 1 ;k<=n ;k++){
       long long int positions = k*k*(k*k-1)/2 - 4*(k-1)*(k-2) ; //arrived at this formula for no of valid positions
       cout<<positions<<"\n" ;                                       // of knights . 
       }
-}                    //problem is that im not able to handle very very large values with this . 
+}                    
