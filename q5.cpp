f8fdf288fe1b3a2fa926030e39514eac784238ba
loak20 <aryankadam2055@gmail.com>
2023-12-01 07:49:29 +0530

    update

---
+++ q5.cpp
@@ -0,0 +1,28 @@
+//hint liya tha 
+#include<iostream>
+using namespace std ;
+int main(){
+    long long int n ;
+    cin>>n ; 
+    if(n==1){
+        cout<<"1" ; 
+    }
+    else if(n==2 or n==3){
+        cout<<"NO SOLUTION" ; 
+    }
+    else if(n==4){
+        cout<<"3"<<" "<<"1"<<" "<<"4"<<" "<<"2" ; 
+    }
+    else{
+        for (int i = n; i>0; i=i-2)
+        {
+            cout<<i<<" " ;                       //writing numbers by the difference of 2  . 
+        }
+        for (int j = n-1; j > 0;j=j-2)
+        {
+            cout<<j<<" " ; 
+        }
+        
+        
+    }
+}
