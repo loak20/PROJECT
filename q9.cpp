e17e6c892edf9590c4919810a31e1159dfc60f95
loak20 <aryankadam2055@gmail.com>
2023-12-04 16:11:06 +0530

    update

--- q9.cpp
+++ q9.cpp
@@ -1,1 +1,12 @@
-
+#include<iostream>
+using namespace std ;
+int main () {
+    long long int n ,a=1 , m = 1000000007; 
+    
+    cin>> n ;
+ 
+    for(long long int i = 0 ; i<n;i++){
+a= (2*a)%m ;
+    } 
+    cout<<a ; 
+}
